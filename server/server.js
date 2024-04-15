b6542885b11ca59f4542ddba6745e6e6bf3caae5
sisindrik <sisindrik@lyrostech.com>
2024-04-15 23:50:17 +0530

    intial commit

--- server/server.js
+++ server/server.js
@@ -1,11 +1,12 @@
+
 import express from 'express';
 import axios from 'axios';
+import bodyParser from 'body-parser';
 
 const app = express();
 const PORT = 3000;
 
-
-app.use(express.json());
+app.use(bodyParser.json());
 
 app.post('/api/auth/signup', async (req, res) => {
     try {
@@ -17,7 +18,6 @@ app.post('/api/auth/signup', async (req, res) => {
     }
 });
 
-
 app.post('/api/auth/login', async (req, res) => {
     try {
         const response = await axios.post('https://instagram-express-app.vercel.app/api/auth/login', req.body);
@@ -28,20 +28,22 @@ app.post('/api/auth/login', async (req, res) => {
     }
 });
 
+
+
 app.delete('/api/auth/logout', async (req, res) => {
     try {
-        const response = await axios.delete('https://instagram-express-app.vercel.app/api/auth/logout');
-        res.json(response.data);
+        res.json({ message: 'Logged out successfully' });
     } catch (error) {
-        console.error('Log Out Error:', error.response.data);
-        res.status(error.response.status).json(error.response.data);
+        console.error('Logout Error:', error.message);
+        res.status(500).json({ error: 'Internal server error' });
     }
 });
 
 
+
 app.post('/api/post/upload', async (req, res) => {
     try {
-       
+      
         res.json({ message: 'Image uploaded successfully' });
     } catch (error) {
         console.error('Upload Error:', error.message);
@@ -49,7 +51,7 @@ app.post('/api/post/upload', async (req, res) => {
     }
 });
 
+// Start server
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
-
